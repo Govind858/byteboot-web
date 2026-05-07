37806d9d8392f21020ae53b5e57090d909276ebd
Govind Jayakumar <govindjayakumar858@gmail.com>
2026-05-07 10:31:18 +0530

    updated mail api

--- src/APIs/Axios.tsx
+++ src/APIs/Axios.tsx
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 // Create the Axios instance
 const apiClient = axios.create({
-  // baseURL: 'https://backend.byteboot.in/',
-  baseURL: 'http://localhost:3000/',
+  baseURL: 'https://backend.byteboot.in/',
+  // baseURL: 'http://localhost:3000/',
   timeout: 5000, // Optional: time out after 5 seconds
   headers: {
     'Content-Type': 'application/json'
